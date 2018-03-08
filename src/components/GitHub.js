077eb8722f73b5e120e35f3eab371fb3cd57a870
Piotr Wiercinski <piotrwier@o2.pl>
2018-03-08 08:51:02 +0100

    first request done

--- src/components/GitHub.js
+++ src/components/GitHub.js
@@ -4,6 +4,17 @@ const urlForUsername = username =>
 	`https://api.github.com/users/${username}`
 
 class GitHub extends Component {
+	
+	componentDidMount() {
+		fetch(urlForUsername(this.props.username))
+		.then(d => d.json())
+		.then(d => {
+			this.setState({
+				githubData: d
+			})
+		});
+	}
+
   render() {
     return (
   		<div>
